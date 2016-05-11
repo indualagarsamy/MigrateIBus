81c01f1f1337fbb441fe4c9d0d36a4e232814177
Sean Feldman <feldman.sean@gmail.com>
2016-05-10 22:15:22 -0600

    working ninject injection of IMessageSession where IBus was previously used

--- MigrateIBus/NsbHostUsingV6Core/EndpointConfig.cs
+++ MigrateIBus/NsbHostUsingV6Core/EndpointConfig.cs
@@ -1,10 +1,12 @@
-
 namespace NsbHostUsingV6Core
 {
+    using Ninject;
     using NServiceBus;
 
     public class EndpointConfig : IConfigureThisEndpoint
     {
+        internal static StandardKernel Kernel = new StandardKernel();
+        
         public void Customize(EndpointConfiguration endpointConfiguration)
         {
             //TODO: NServiceBus provides multiple durable storage options, including SQL Server, RavenDB, and Azure Storage Persistence.
@@ -19,6 +21,7 @@ namespace NsbHostUsingV6Core
             // that you start with a shared audit queue for all your endpoints for easy integration with ServiceControl.
             endpointConfiguration.AuditProcessedMessagesTo("audit");
 
+            endpointConfiguration.UseContainer<NinjectBuilder>(c => c.ExistingKernel(Kernel));
         }
     }
 }
